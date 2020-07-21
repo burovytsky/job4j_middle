59bbe6baddbb62d5ae309848d31d66d4ccb87ff7
burovytsky <esfero1505@gmail.com>
2020-07-21 19:04:54 +0300

    3. Прерывание нити [ #330092 ]

---
+++ src/main/java/ru/job4j/multithreading/ConsoleProgress.java
@@ -0,0 +1,31 @@
+package ru.job4j.multithreading;
+
+public class ConsoleProgress implements Runnable {
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        Thread.sleep(3000);
+        progress.interrupt();
+    }
+
+    @Override
+    public void run() {
+        String load = "\\";
+        while (!Thread.currentThread().isInterrupted()) {
+            try {
+                System.out.print("\r load... " + load);
+                load = switch (load) {
+                    case "\\" -> "|";
+                    case "|" -> "/";
+                    case "/" -> "-";
+                    default -> "\\";
+                };
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
