31e48898014b9de10770a37cbe93536e0d961271
burovytsky <esfero1505@gmail.com>
2020-07-24 20:16:10 +0300

    1.1. Состояние нити. [#330088]

--- src/main/java/ru/job4j/multithreading/resource_synchronization/ParseFile.java
+++ src/main/java/ru/job4j/multithreading/resource_synchronization/ParseFile.java
@@ -15,31 +15,37 @@ public class ParseFile {
     }
 
     public synchronized String getContent() throws IOException {
-        InputStream i = new FileInputStream(file);
-        StringBuilder output = new StringBuilder();
-        int data;
-        while ((data = i.read()) > 0) {
-            output.append((char) data);
-        }
-        return output.toString();
-    }
-
-    public synchronized String getContentWithoutUnicode() throws IOException {
-        InputStream i = new FileInputStream(file);
-        StringBuilder output = new StringBuilder();
-        int data;
-        while ((data = i.read()) > 0) {
-            if (data < 0x80) {
+        StringBuilder output;
+        try (InputStream i = new FileInputStream(file)) {
+            output = new StringBuilder();
+            int data;
+            while ((data = i.read()) > 0) {
                 output.append((char) data);
             }
         }
         return output.toString();
     }
 
+    public synchronized String getContentWithoutUnicode() throws IOException {
+        StringBuilder output;
+        try (InputStream i = new FileInputStream(file)) {
+            output = new StringBuilder();
+            int data;
+            while ((data = i.read()) > 0) {
+                if (data < 0x80) {
+                    output.append((char) data);
+                }
+            }
+        }
+        return output.toString();
+
+    }
+
     public synchronized void saveContent(String content) throws IOException {
-        OutputStream o = new FileOutputStream(file);
-        for (int i = 0; i < content.length(); i += 1) {
-            o.write(content.charAt(i));
+        try (OutputStream o = new FileOutputStream(file)) {
+            for (int i = 0; i < content.length(); i += 1) {
+                o.write(content.charAt(i));
+            }
         }
     }
 }
