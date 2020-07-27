d0fde1bd4c02d04d34c4ce764b44d37288313d41
Constantine Burovytsky <esfero1505@gmail.com>
2020-07-27 10:13:56 +0300

    3. Класс хранилища пользователей UserStorage [#330103]

--- src/main/java/ru/job4j/multithreading/resource_synchronization/UserStorage.java
+++ src/main/java/ru/job4j/multithreading/resource_synchronization/UserStorage.java
@@ -1,5 +1,6 @@
 package ru.job4j.multithreading.resource_synchronization;
 
+import net.jcip.annotations.GuardedBy;
 import net.jcip.annotations.ThreadSafe;
 
 import java.util.ArrayList;
@@ -8,17 +9,22 @@ import java.util.List;
 
 @ThreadSafe
 public class UserStorage {
-    private volatile List<User> users = new ArrayList<>();
+    @GuardedBy("this")
+    private final List<User> users = new ArrayList<>();
 
-    public boolean add(User user) {
-        return users.add(new User(user.getId(), user.getBalance()));
+    public synchronized boolean add(User user) {
+        if (findById(user.getId()) == null) {
+            users.add(new User(user.getId(), user.getBalance()));
+            return true;
+        }
+        return false;
     }
 
-    public boolean update(User user) {
+    public synchronized boolean update(User user) {
         return delete(user) && add(user);
     }
 
-    public boolean delete(User user) {
+    public synchronized boolean delete(User user) {
         return users.remove(findById(user.getId()));
     }
 
@@ -34,7 +40,7 @@ public class UserStorage {
         return false;
     }
 
-    public User findById(int id) {
+    public synchronized User findById(int id) {
         User rsl = null;
         for (User user : users) {
             if (user.getId() == id) {
@@ -45,11 +51,60 @@ public class UserStorage {
         return rsl;
     }
 
-    public List<User> findAll() {
+    public synchronized List<User> findAll() {
         List<User> rsl = new ArrayList<>();
         for (User user : users) {
             rsl.add(new User(user.getId(), user.getBalance()));
         }
         return rsl;
     }
+
+//    @GuardedBy("this")
+//    private List<User> users = new ArrayList<>();
+//
+//    public synchronized boolean add(User user) {
+//        if (findById(user.getId()) == null) {
+//            users.add(user);
+//            return true;
+//        }
+//        return false;
+//    }
+//
+//    public synchronized boolean update(User user) {
+//        return delete(user) && add(user);
+//    }
+//
+//    public synchronized boolean delete(User user) {
+//        return users.remove(findById(user.getId()));
+//    }
+//
+//    public synchronized boolean transfer(int fromId, int toId, int amount) {
+//        User userFrom = findById(fromId);
+//        User userTo = findById(toId);
+//        if (userFrom.withdraw(amount) && userTo != null) {
+//            userTo.deposit(amount);
+//            update(userFrom);
+//            update(userTo);
+//            return true;
+//        }
+//        return false;
+//    }
+//
+//    public synchronized User findById(int id) {
+//        User rsl = null;
+//        for (User user : users) {
+//            if (user.getId() == id) {
+//                return user;
+//            }
+//        }
+//        return rsl;
+//    }
+//
+//    public synchronized List<User> findAll() {
+//        List<User> rsl = new ArrayList<>();
+//        for (User user : users) {
+//            rsl.add(new User(user.getId(), user.getBalance()));
+//        }
+//        return rsl;
+//    }
 }
