eab7c3d6a949dd16f58b40a81a9cc81f26b9aba7
burovytsky <esfero1505@gmail.com>
2020-07-29 18:54:17 +0300

    2. Обеспечить остановку потребителя. [#330082]

---
+++ src/main/java/ru/job4j/multithreading/wait_notify_notifyAll/ParallelSearch.java
@@ -0,0 +1,35 @@
+package ru.job4j.multithreading.wait_notify_notifyAll;
+
+public class ParallelSearch {
+    public static void main(String[] args){
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(1);
+        final Thread consumer = new Thread(
+                () -> {
+                    while (!Thread.currentThread().isInterrupted()) {
+                        try {
+                            queue.poll();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            Thread.currentThread().interrupt(); } } }
+        );
+        consumer.start();
+        new Thread(
+                () -> {
+                    for (int index = 0; index != 10; index++) {
+                        try {
+                            queue.offer(index);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    consumer.interrupt();
+                }
+
+        ).start();
+    }
+}
